8bec2b6c1143e4ca104dd5509c9aa8aa3911a2d0
Kelly Shearer <kellyshearer11@gmail.com>
2020-10-28 19:29:55 +0000

    Lab done

--- src/components/create.js
+++ src/components/create.js
@@ -3,10 +3,77 @@ import  React from 'react';
 // Create component 
 export class Create extends React.Component{
 
+    constructor(){
+        super();
+        // binding events
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeYear = this.onChangeYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster:''
+        }
+    }
+    onChangeTitle(e){
+        this.setState({
+            Title: e.target.value
+        });
+    }
+
+    onChangeYear(e){
+        this.setState({
+            Year: e.target.value
+        });
+    }
+
+    onChangePoster(e){
+        this.setState({
+            Poster: e.target.value
+        });
+    }
+
+    // onsubmit method
+    onSubmit(e){
+        e.preventDefault();
+        alert("Movie: " + this.state.Title + " " + this.state.Year + " " + this.state.Poster);
+    }
     render(){
         return(
-            <div>
-                <h1>This is the create component</h1>
+            // div form to input movie title, year, poster
+            <div className='App'>
+               <form onSubmit={this.onSubmit}>
+                   <div className='form-group'>
+                       <label> Add movie Title </label>
+                       <input type='text' 
+                       className='form-control'
+                       value={this.state.Title}
+                       onChange={this.onChangeTitle}></input>
+                   </div>
+                   <div className='form-group'>
+                       <label> Add movie Year </label>
+                       <input type='text' 
+                       className='form-control'
+                       value={this.state.Year}
+                       onChange={this.onChangeYear}></input>
+                   </div>
+                   <div className='form-group'>
+                       <label>Add movie poster </label>
+                       <textarea type='text'
+                       className='form-control'
+                       value={this.state.Poster}
+                       onChange={this.onChangePoster}>
+                       </textarea>
+
+                   </div>
+                   <div className="form-group">
+                       <input type='submit'
+                       value='Add Movie'
+                       className='btn btn-primary'></input>
+                   </div>
+               </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,41 +1,34 @@
-import  React from 'react';
+import React from 'react';
 import { Movies } from './movies';
+import axios from 'axios';
 
 // Read component 
-export class Read extends React.Component{
+export class Read extends React.Component {
 
     // state object called movies
-    // json data for movie components
+    // Install axios to call get requestb for Json blob/ API
+    // pulling the data by using response method
+    // arrow function
 
     state = {
-        movies: [
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]
-            
-    };
+        movies: []
 
-    render(){
-        return(
+    };
+    // component lifecycle hook
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+            .then(
+                (response) => {
+                    this.setState({ movies: response.data.Search })
+                }
+            ) // if error above it will be logged to console
+            .catch(
+                (error) => { console.log(error) }
+            );
+    }
+
+    render() {
+        return (
             <div>
                 <h1>This is the read component</h1>
                 <Movies movies={this.state.movies}></Movies>
